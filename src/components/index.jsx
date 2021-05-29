81591f26d5b760972608ddd5c6ab81c8d63f11e6
hoka-hoka <sff1@tpu.ru>
2021-05-29 11:20:43 +0700

    cleanup template

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,32 +1,123 @@
 import React, { Component } from 'react';
-import Button from './Button';
-import Modal from './Modal';
-import styles from './template.scss';
+import { viewMode, baseURL, lang, langData } from '../constants';
+import Sprite from '../common/Sprite';
+import Preloader from '../common/Preloader';
+import Person from './Person';
+import ModalWindow from './ModalWindow';
 
-class Template extends Component {
+import EditPerson from './EditPerson';
+import NewPerson from './NewPerson';
+import './App.scss';
+
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { amountClick: 0 };
+    this.state = {
+      view: viewMode.load,
+      curEmployee: { firstName: '', lastName: '' },
+      persons: [],
+    };
   }
 
+  componentDidMount = () => {
+    this.getData('persons').then((resp) => {
+      this.setState({ persons: resp, view: viewMode.list });
+    });
+  };
+
+  getData = async (method) => {
+    const resp = await fetch(`${baseURL}/api/v1/${method}`);
+    if (!resp?.ok) {
+      this.setState({ error: true });
+      return new Error('Ответ на запрос пустой');
+    }
+    return resp.json();
+  };
+
+  addEmployee = (event) => {
+    event.stopPropagation();
+    this.setState({ view: viewMode.add });
+  };
+
+  editEmployee = (person) => {
+    this.setState({ view: viewMode.edit, curEmployee: person });
+  };
+
+  delEmployee = (index) => {
+    this.setState(({ persons }) => ({
+      persons: persons.filter((_, pos) => pos != index),
+    }));
+  };
+
+  updateState = ({ update } = {}) => {
+    if (update) {
+      this.forceUpdate();
+    }
+    return this.state;
+  };
+
   render() {
-    const { amountClick } = this.state;
+    const { view, curEmployee, persons } = this.state;
     return (
       <>
-        <div className={styles.template}>
-          <div className={styles.header}>
-            <div className={styles.top}>
-              <Button amountClick={amountClick} setData={this.state} />
-            </div>
-            <div className={styles.bottom}>
-              <Modal name="title" />
-              <Button />
-            </div>
-          </div>
+        {view == viewMode.load ? <Preloader /> : false}
+        <div
+          className={`employees${
+            view == viewMode.list ? '' : ' employees_hidden'
+          }`}
+        >
+          <table className="employees__list">
+            <thead>
+              <tr className="employees__line">
+                <th />
+                <th className="employees__fname" colSpan="1">
+                  {lang[langData.firstName]}
+                </th>
+                <th className="employees__lname" colSpan="2">
+                  {lang[langData.lastName]}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {persons.map((person, index) => (
+                <Person
+                  userData={person}
+                  key={person.id}
+                  editEmployee={() => this.editEmployee(person)}
+                  delEmployee={() => this.delEmployee(index)}
+                />
+              ))}
+            </tbody>
+          </table>
+          <button
+            className="employees__add"
+            type="button"
+            onClick={this.addEmployee}
+          >
+            {lang[langData.add]}
+          </button>
         </div>
+        {view == viewMode.add && (
+          <ModalWindow
+            title={lang[langData.create]}
+            updateState={({ update }) => this.updateState({ update })}
+          >
+            <NewPerson />
+          </ModalWindow>
+        )}
+        {view == viewMode.edit && (
+          <ModalWindow
+            title={lang[langData.edit]}
+            updateState={({ update }) => this.updateState({ update })}
+          >
+            <EditPerson
+              firstName={curEmployee.firstName}
+              lastName={curEmployee.lastName}
+            />
+          </ModalWindow>
+        )}
+        <Sprite />
       </>
     );
   }
 }
-
-export default Template;
